4fb1ae6f337c08db54ddcc6a90ae7f002126c0c4
Sergey Lebedev <superlebedew@gmail.com>
2023-01-17 13:52:09 +0300

    update

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import psycopg2
 
-conn = psycopg2.connect(database='client_db', user='postgres', password='*****')
-
 
 def create_table(cursor, name, structure):
-    cursor.execute(f'create table if not exists {name}( id SERIAL primary key, {structure});')
+    gry = f"create table if not exists {name}(id SERIAL primary key, {structure});"
+    cursor.execute(gry)
     conn.commit()
 
 
 def new_client(cursor, client_name, client_surname_name, client_email):
-    cursor.execute(f'INSERT INTO client(name, surname_name, email) '
-                   f'values{client_name, client_surname_name, client_email};')
+    cursor.execute("""
+                    INSERT INTO client(name, surname_name, email) 
+                    values(%s,  %s, %s);
+                    """, (client_name, client_surname_name, client_email))
     conn.commit()
     print('Клиент добавлен')
 
@@ -30,26 +31,27 @@ def new_number(cursor, number, client_email):
         if c == None:
             print('Клиента с таким майлом нет ')
         else:
-            cursor.execute(f'INSERT INTO phone(number, client_id ) values{number, c[1]};')
-            conn.commit()
+            cursor.execute(""" INSERT INTO phone(number, client_id ) 
+            values(%s, %s);""" % (number, c[1])
+                           )
+
             print(f'Номер добавлен к клиенту {c[0]} {c[2]}')
 
     else:
         print(f'Номер уже существует в базе!')
 
 
-def change_data(cursor, name, surname_name, email, new_mail):
-    cursor.execute("""
-        select id from client where email = %s;
-        """, (email,))
-    x = cursor.fetchone()
-    if x == None:
-        print('Клиента с таким майлом нет')
-    else:
-        cursor.execute('update client set name= %s, surname_name = %s, email = %s where id = %s;'
-                      , (name, surname_name, new_mail, x[0]))
-        print('Данные изменены ')
-    conn.commit()
+def change_data(cursor, email, dict_t: dict):
+    d = {}
+    for key, val in dict_t.items():
+        if val.strip():
+            d[key] = val
+    for key, val in d.items():
+        v = (val, email)
+        gry = (f"""update client set {key} = %s  where email = %s;
+    """)
+        cursor.execute(gry, v)
+        print("Данные изменены")
 
 
 def delete_number(cursor, email, number):
@@ -89,40 +91,46 @@ def delete_client(cursor, email):
         print("Клиент удален")
 
 
-def search_client(cursor, search):
-    cursor.execute("""
-                select name, surname_name, email, p.number from client c
-                left join phone p on c.id = p.client_id
-                where name ilike %s or surname_name ilike %s or email ilike %s or p.number ilike %s;
-                """, (search, search, search, search))
+def search_client(cursor, search: dict):
+    d = {}
+    for key, val in search.items():
+        if val.strip():
+            d[key] = val
+    for key, val in d.items():
+        v = (val,)
+        gry = (f"""
+        select name, surname_name, email, p.number from client c
+        left join phone p on c.id = p.client_id
+        where {key} ilike %s;
+        """)
+        cursor.execute(gry, v)
+        print(cursor.fetchall()[0])
 
-    y = cursor.fetchall()
-    print(y)
 
-
-cur = conn.cursor()
-
+if __name__ == "__main__":
+    with psycopg2.connect(database='client_db', user='postgres', password='wifi1993+') as conn:
+        cur = conn.cursor()
 #Функция, создающая структуру БД (таблицы)
-# structure_1 = 'name VARCHAR (50) not null, surname_name VARCHAR (50) not null, email VARCHAR unique not null '
-# structure_2 = 'number VARCHAR (50), client_id integer not null references client(id)'
-# create_table(cur, 'client', structure_1)
-# create_table(cur, 'phone', structure_2)
+        #structure_1 = 'name VARCHAR (50) not null, surname_name VARCHAR (50) not null, email VARCHAR unique not null '
+    #     structure_2 = 'number VARCHAR (50), client_id integer not null references client(id)'
+        # create_table(cur, 'client', structure_1)
+    #     create_table(cur, "test_2", structure_2)
 #Функция, позволяющая добавить нового клиента
-# new_client(cur, 'Anzor', 'les', '002584@gmail.com')
+        #new_client(cur, 'An', 's', '4@gmail.com')
 
 #Функция, позволяющая добавить телефон для существующего клиента
-# new_number(cur, '2345678', 'olo@gmail.com')
+        # new_number(cur, '55890', '123@gmail.com')
 
 #Функция, позволяющая изменить данные о клиенте
-# change_data(cur, 'Lex', 'f', '343434o@gmail.com', '123@gmail.com')
+        # new_dict = {'name': " lexa", 'surname_name': "maj", 'email': "  "}
+        # change_data(cur, 'm@gmail.com', new_dict)
 
 #Функция, позволяющая удалить телефон для существующего клиента
-# delete_number(cur, 'zamd@gmail.com', '1002')
+        # delete_number(cur, 'zamd@gmail.com', '1002')
 
 #Функция, позволяющая удалить существующего клиента
-# delete_client(cur, 'Les@gmail.com')
+        # delete_client(cur, 'Les@gmail.com')
 
 #Функция, позволяющая найти клиента по его данным (имени, фамилии, email-у или телефону)
-# search_client(cur, 'f')
-
-conn.close()
+        # d_1 = {"name": "", 'surname_name': "", 'email': "", 'number': "89434300400"}
+        # search_client(cur, d_1)
